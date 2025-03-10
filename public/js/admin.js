10bb5225475a2fecac3883c4c2ef394b96b96876
FIRST_NAME LAST_NAME <EMAIL_ADDRESS>
2025-03-11 00:37:34 +0100

    message

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,141 @@
-let haalitemsOp = JSON.parse(localStorage.getItem("opslaanJSON"));
+// let haalitemsOp = JSON.parse(localStorage.getItem("opslaanJSON"));
+
+// let voegProducten = document.getElementById("voegProducten");
+// let modalShowProducten = document.querySelector(".modal-body");
+// modalShowProducten.innerHTML = `
+// <h1>voeg product gegevens toe om het gevoegd te worden</h1>
+// <input id="nameProduct" class="col-12 form-control" type="text" placeholder="voeg hier de naam toe">
+// <input id="urlFoto" class="col-12 form-control" type="text" placeholder="url van foto of leeg automatish afb">
+// <input id="prijsProduct" 
+// class="col-12 form-control" type="text" placeholder="prijs van de product alleen in getallen geen Euro teken">
+// <input id="vink1Product" class="col-12 form-control" type="text" placeholder="informatie over je product">
+// <input id="vink2Product" class="col-12 form-control" type="text" placeholder="informatie over je product">
+
+// `;
+// console.log(JSON.parse(localStorage.getItem("opslaanJSON")));
+// let buttonSaveNewProduct = document.getElementById("buttonSaveNewProduct");
+// buttonSaveNewProduct.addEventListener("click", () => {
+//     let nameProduct = document.getElementById("nameProduct");
+//     let urlFoto = document.getElementById("urlFoto");
+//     let prijsProduct = document.getElementById("prijsProduct");
+//     let vink1Product = document.getElementById("vink1Product");
+//     let vink2Product = document.getElementById("vink2Product");
+//     let pushNewProduct = {
+//         name: nameProduct.value,
+//         url: urlFoto.value,
+//         prijs: prijsProduct.value,
+//         vink1: vink1Product.value,
+//         vink2: vink2Product.value,
+//         aantal: 0,
+//     };
+//     opslaanLOCAL(pushNewProduct);
+// });
+// // producten nu wijzigen verwijderen
+// let producteDieAanwezigZijn = document.getElementById("producten");
+
+
+// let rowContainer = document.createElement("div");
+// rowContainer.classList.add("row");
+
+// for (let i = 0; i < haalitemsOp.length; i++) {
+//     let card = document.createElement("div");
+//     card.classList.add("col-md-4");
+//     card.innerHTML = `
+//     <div class="card">
+//         <div class="row g-3 p-3">
+//             <div class="col-md-12">
+//                 <h2>naam: ${haalitemsOp[i].name}</h2>
+//                 <p>prijs: €${haalitemsOp[i].prijs}</p>
+//                 <p>vink1: ${haalitemsOp[i].vink1}</p>
+//                 <p>vink2: ${haalitemsOp[i].vink2}</p>
+//             <button type="button" class="btn btn-success wijzigProducten"
+//              data-index="${i}" data-bs-target="#wijzigProduct" data-bs-toggle="modal">  Wijzig het</button>
+//               <button type="button" class="btn btn-danger verwijderProducten"
+//                  data-index="${i}"  data-bs-toggle="modal" data-bs-target="#bevestigd">verwijder het</button>
+//             </div>
+//         </div>
+//     </div>
+//     `;
+
+//     rowContainer.appendChild(card);
+// }
+// producteDieAanwezigZijn.appendChild(rowContainer);
+// // nu producten wordne verwijderd als je daarvoor kiest
+// let verwijderProducten = document.querySelectorAll(".verwijderProducten");
+// verwijderProducten.forEach(button => {
+//     button.addEventListener("click", (event) => {
+//         let bevstigVERWIJDER = document.getElementById("bevstigVERWIJDER");
+//         bevstigVERWIJDER.addEventListener("click", () => {
+//             let index = event.target.getAttribute("data-index");
+//             haalitemsOp.splice(index, 1);
+//             localStorage.setItem("opslaanJSON", JSON.stringify(haalitemsOp));
+//             window.location.reload();
+//         });
+//     });
+// });
+// // nu producten wijzigem als het lukt
+// let wijzigProducten = document.querySelectorAll(".wijzigProducten");
+// wijzigProducten.forEach(button => {
+//     button.addEventListener("click", (event) => {
+//         let index = event.target.getAttribute("data-index");
+//         let bodyModal = document.querySelector(".bodyModal");
+//         bodyModal.innerHTML = `
+//         <div class="">
+//         <h6>wat je wilt wijzigen druk er op en typ wat je wilt wijzigen en sla het op</h6>
+//         <input id="naamProductWijzig" type="text" class="form-control" placeholder="naam:
+//          ${haalitemsOp[index].name}">
+//         <input id="prijsProductWijzig" type="text" class="form-control" placeholder="prijs:
+//          ${haalitemsOp[index].prijs}">
+//         <input id="vink1ProductWijzig" type="text" class="form-control" placeholder="vink1:
+//          ${haalitemsOp[index].vink1}">
+//          <input id="vink2ProductWijzig" type="text" class="form-control" placeholder="vink2:
+//           ${haalitemsOp[index].vink2}">
+//         </div>
+//         `;
+//         let saveProductWijzig = document.getElementById("saveProductWijzig");
+//         if (saveProductWijzig) {
+//             saveProductWijzig.addEventListener("click", () => {
+//                 let nieuweNaam = document.getElementById("naamProductWijzig").value;
+//                 let nieuwePrijs = document.getElementById("prijsProductWijzig").value;
+//                 let nieuweVink1 = document.getElementById("vink1ProductWijzig").value;
+//                 let nieuweVink2 = document.getElementById("vink2ProductWijzig").value;
+//                 let gewijzigdeProduct = {
+//                     name: nieuweNaam || haalitemsOp[index].name,
+//                     prijs: nieuwePrijs || haalitemsOp[index].prijs,
+//                     vink1: nieuweVink1 || haalitemsOp[index].vink1,
+//                     vink2: nieuweVink2 || haalitemsOp[index].vink2,
+//                     aantal: haalitemsOp[index].aantal,
+//                 };
+//                 opslaanLOCAL(gewijzigdeProduct, index);
+//             });
+//         }
+//     });
+// });
+// // hier word het opgeslagen
+// function opslaanLOCAL(opslaanNewProduct, index = null) {
+//     let producten = JSON.parse(localStorage.getItem("opslaanJSON")) || [];
+//     if (index == null) {
+//         producten.push(opslaanNewProduct);
+//     } else {
+//         producten[index] = opslaanNewProduct;
+//     }
+//     localStorage.setItem("opslaanJSON", JSON.stringify(producten));
+//     console.log(JSON.parse(localStorage.getItem("opslaanJSON")));
+//     window.location.reload();
+// }
+// let winkelwagenBestellingen = document.getElementById("winkelwagenBestellingen");
+// let getWinkelwagenBestellingen = JSON.parse(localStorage.getItem("winkelwagen")) || [];
+// console.log(getWinkelwagenBestellingen);
+// getWinkelwagenBestellingen.forEach(element => {
+//     winkelwagenBestellingen.innerHTML += `
+//   <div class="card col-md-4"> 
+//   <p>naam: ${element.name}</p>
+//    <p>aantal:${element.aantal}</p>
+//    <hr> 
+//    </div> `;
+// });
+
+let haalitemsOp = JSON.parse(localStorage.getItem("opslaanJSON")) || [];
 
 let voegProducten = document.getElementById("voegProducten");
 let modalShowProducten = document.querySelector(".modal-body");
@@ -6,13 +143,13 @@ modalShowProducten.innerHTML = `
 <h1>voeg product gegevens toe om het gevoegd te worden</h1>
 <input id="nameProduct" class="col-12 form-control" type="text" placeholder="voeg hier de naam toe">
 <input id="urlFoto" class="col-12 form-control" type="text" placeholder="url van foto of leeg automatish afb">
-<input id="prijsProduct" 
-class="col-12 form-control" type="text" placeholder="prijs van de product alleen in getallen geen Euro teken">
+<input id="prijsProduct" class="col-12 form-control" type="text" placeholder="prijs van de product alleen in getallen geen Euro teken">
 <input id="vink1Product" class="col-12 form-control" type="text" placeholder="informatie over je product">
 <input id="vink2Product" class="col-12 form-control" type="text" placeholder="informatie over je product">
-
 `;
-console.log(JSON.parse(localStorage.getItem("opslaanJSON")));
+
+console.log(haalitemsOp);
+
 let buttonSaveNewProduct = document.getElementById("buttonSaveNewProduct");
 buttonSaveNewProduct.addEventListener("click", () => {
     let nameProduct = document.getElementById("nameProduct");
@@ -20,6 +157,7 @@ buttonSaveNewProduct.addEventListener("click", () => {
     let prijsProduct = document.getElementById("prijsProduct");
     let vink1Product = document.getElementById("vink1Product");
     let vink2Product = document.getElementById("vink2Product");
+
     let pushNewProduct = {
         name: nameProduct.value,
         url: urlFoto.value,
@@ -30,9 +168,9 @@ buttonSaveNewProduct.addEventListener("click", () => {
     };
     opslaanLOCAL(pushNewProduct);
 });
-// producten nu wijzigen verwijderen
-let producteDieAanwezigZijn = document.getElementById("producten");
 
+// Producten nu wijzigen en verwijderen
+let producteDieAanwezigZijn = document.getElementById("producten");
 
 let rowContainer = document.createElement("div");
 rowContainer.classList.add("row");
@@ -48,9 +186,9 @@ for (let i = 0; i < haalitemsOp.length; i++) {
                 <p>prijs: €${haalitemsOp[i].prijs}</p>
                 <p>vink1: ${haalitemsOp[i].vink1}</p>
                 <p>vink2: ${haalitemsOp[i].vink2}</p>
-            <button type="button" class="btn btn-success wijzigProducten"
-             data-index="${i}" data-bs-target="#wijzigProduct" data-bs-toggle="modal">  Wijzig het</button>
-              <button type="button" class="btn btn-danger verwijderProducten"
+                <button type="button" class="btn btn-success wijzigProducten"
+                 data-index="${i}" data-bs-target="#wijzigProduct" data-bs-toggle="modal">  Wijzig het</button>
+                <button type="button" class="btn btn-danger verwijderProducten"
                  data-index="${i}"  data-bs-toggle="modal" data-bs-target="#bevestigd">verwijder het</button>
             </div>
         </div>
@@ -60,7 +198,8 @@ for (let i = 0; i < haalitemsOp.length; i++) {
     rowContainer.appendChild(card);
 }
 producteDieAanwezigZijn.appendChild(rowContainer);
-// nu producten wordne verwijderd als je daarvoor kiest
+
+// Nu producten worden verwijderd als je daarvoor kiest
 let verwijderProducten = document.querySelectorAll(".verwijderProducten");
 verwijderProducten.forEach(button => {
     button.addEventListener("click", (event) => {
@@ -73,7 +212,8 @@ verwijderProducten.forEach(button => {
         });
     });
 });
-// nu producten wijzigem als het lukt
+
+// Nu producten wijzigen als het lukt
 let wijzigProducten = document.querySelectorAll(".wijzigProducten");
 wijzigProducten.forEach(button => {
     button.addEventListener("click", (event) => {
@@ -82,14 +222,10 @@ wijzigProducten.forEach(button => {
         bodyModal.innerHTML = `
         <div class="">
         <h6>wat je wilt wijzigen druk er op en typ wat je wilt wijzigen en sla het op</h6>
-        <input id="naamProductWijzig" type="text" class="form-control" placeholder="naam:
-         ${haalitemsOp[index].name}">
-        <input id="prijsProductWijzig" type="text" class="form-control" placeholder="prijs:
-         ${haalitemsOp[index].prijs}">
-        <input id="vink1ProductWijzig" type="text" class="form-control" placeholder="vink1:
-         ${haalitemsOp[index].vink1}">
-         <input id="vink2ProductWijzig" type="text" class="form-control" placeholder="vink2:
-          ${haalitemsOp[index].vink2}">
+        <input id="naamProductWijzig" type="text" class="form-control" placeholder="naam: ${haalitemsOp[index].name}">
+        <input id="prijsProductWijzig" type="text" class="form-control" placeholder="prijs: ${haalitemsOp[index].prijs}">
+        <input id="vink1ProductWijzig" type="text" class="form-control" placeholder="vink1: ${haalitemsOp[index].vink1}">
+        <input id="vink2ProductWijzig" type="text" class="form-control" placeholder="vink2: ${haalitemsOp[index].vink2}">
         </div>
         `;
         let saveProductWijzig = document.getElementById("saveProductWijzig");
@@ -111,7 +247,8 @@ wijzigProducten.forEach(button => {
         }
     });
 });
-// hier word het opgeslagen
+
+// Hier wordt het opgeslagen
 function opslaanLOCAL(opslaanNewProduct, index = null) {
     let producten = JSON.parse(localStorage.getItem("opslaanJSON")) || [];
     if (index == null) {
@@ -123,9 +260,11 @@ function opslaanLOCAL(opslaanNewProduct, index = null) {
     console.log(JSON.parse(localStorage.getItem("opslaanJSON")));
     window.location.reload();
 }
+
 let winkelwagenBestellingen = document.getElementById("winkelwagenBestellingen");
 let getWinkelwagenBestellingen = JSON.parse(localStorage.getItem("winkelwagen")) || [];
 console.log(getWinkelwagenBestellingen);
+
 getWinkelwagenBestellingen.forEach(element => {
     winkelwagenBestellingen.innerHTML += `
   <div class="card col-md-4"> 
--- public/js/schema.js
+++ public/js/schema.js
@@ -1,25 +1,141 @@
+// let logo = document.getElementById("logo");
+// logo.addEventListener("click", () => {
+//     window.location.href = "index.html";
+// });
+
+// // fetch het json bestand dus als iemand via de admiun het reset krijg hij dat te zien
+// let getItemJSON;
+// async function fetchJSONProducten() {
+//     // test schema
+//     try {
+//         let fetchJSON = await fetch("https://school-project-git-main-yasins-projects-b82e3aad.vercel.app/json/schema.json");
+//         if (!fetchJSON.ok) {
+//             throw new Error("er is een fout opgetreden" + fetchJSON.status);
+//         }
+//         let makeJSON = await fetchJSON.json();
+//         // we hebben het gefetcht dus nu gaan we het ophalen en bewerken maar eerst gaan we deze informatie ook oplsaan in localstroage
+//         // dus we gaan door met localstorage 1 keer opahelen is genoeg want dit gaat nooit meer gebeuren
+//          getItemJSON = JSON.parse(localStorage.getItem("opslaanJSON"));
+//         // if(makeJSON){
+//         //     getItemJSON = makeJSON;
+//         // }
+//         // nu gaan we die card ophalen dus  via de admin die gepusht word, word hier weergegeve
+//         if (getItemJSON && getItemJSON.length > 0) {
+//             productenOphalen();
+//             winkelmandje(); 
+//         } else {
+//             console.error("Er zijn geen producten beschikbaar in localStorage.");
+//         }
+//     } catch (error) {
+//         console.error(error.message);
+//     }
+// }
+// // producten ophalen beneden
+// async function productenOphalen() {
+//     const HTMLdynamish = document.getElementById("deel");
+//         for (let i = 0; i < getItemJSON.length; i++) {
+//             let imageURL = getItemJSON[i].url || "../imagess/schema.png";
+//             const card = document.createElement("div");
+//             card.classList.add("col-md-3");
+//             card.innerHTML = `
+//           <div class="card">
+//               <div class="row g-3">
+//                   <div class="col-md-12">
+//                       <h3 class="text-center">${getItemJSON[i].name}</h3>
+//                         <div class="col-md-12">
+//                            <img src="${imageURL}" alt="${getItemJSON[i].name}" class="img-fluid">
+//                         </div>
+//                       <p class="text-center">Prijs: € ${getItemJSON[i].prijs}</p>
+//                       <p class="text-center">✔️${getItemJSON[i].vink1}</p>
+//                       <p class="text-center">✔️${getItemJSON[i].vink2}</p>
+//                      <div class="d-flex justify-content-center">
+//                       <button type="button" class="btn btn-warning buttonVanWinkelmandje
+//                        text-center col-md-8" data-index="${i}">
+//                       In winkelmandje</button></div>
+//                   </div>
+//               </div>
+//           </div>
+//       `;
+//             HTMLdynamish.appendChild(card);
+//         }
+// }
+// //? winkelmandje benden 
+// // items opgehaald nu de winkelmandje
+// async function winkelmandje() {
+//     let winkelmandjeButton = document.querySelectorAll(".buttonVanWinkelmandje");
+//     winkelmandjeButton.forEach(button => {
+//         button.addEventListener("click", (event) => {
+//             let index = event.target.getAttribute("data-index");
+//             let product = getItemJSON[index];
+//             let winkelwagen = JSON.parse(localStorage.getItem("winkelwagen")) || [];
+//             let productIndex = winkelwagen.findIndex(item => item.name == product.name);
+//             console.log(productIndex);
+//             if (productIndex !== -1) {
+//                 winkelwagen[productIndex].aantal++;
+//             } else {
+//                 product.aantal = 1;
+//                 winkelwagen.push(product);
+//             }
+//             localStorage.setItem("winkelwagen", JSON.stringify(winkelwagen));
+//             alert(`${product.name} toegevoegd man`);
+//         });
+//     });
+//     let winkelwagenKnop = document.getElementById("winkelwagenKnop");
+//     let modalBODY = document.getElementById("modalBODY");
+//     winkelwagenKnop.addEventListener("click", () => {
+//         let winkelwagen = JSON.parse(localStorage.getItem("winkelwagen")) || [];
+//         modalBODY.innerHTML = "<h5 class='text-center'>Winkelwagen</h5>";
+//         if (winkelwagen.length === 0) {
+//             modalBODY.innerHTML += "<p>Je winkelwagen is leeg</p>";
+//         } else {
+//             winkelwagen.forEach(item => {
+//                 modalBODY.innerHTML += `
+//                     <div class="product-in-winkelwagen">
+//                         <h6>${item.name} (x${item.aantal})</h6>
+//                         <p>prijs: ${item.prijs}</p>
+//                         <p>totaal prijs: € ${item.prijs * item.aantal}</p>
+//                         <hr>
+//                     </div>
+//                 `;
+//             });
+//             modalBODY.innerHTML += `<button class="btn btn-danger" id="leegWinkelwagen">leeg winkelwagen</button>`;
+//             let leegWinkelwagen = document.getElementById("leegWinkelwagen");
+//             leegWinkelwagen.addEventListener("click", () => {
+//                 localStorage.removeItem("winkelwagen");
+//                 window.location.reload();
+//             });
+//         }
+//     });
+// }
+// fetchJSONProducten();
+// function opslaanLOCAL(opslaan) {
+//     localStorage.setItem("opslaanJSON", JSON.stringify(opslaan));
+//     console.log(JSON.parse(localStorage.getItem("opslaanJSON")));
+// }
+
+
 let logo = document.getElementById("logo");
 logo.addEventListener("click", () => {
     window.location.href = "index.html";
 });
 
-// fetch het json bestand dus als iemand via de admiun het reset krijg hij dat te zien
+// Fetch het JSON bestand dus als iemand via de admin het reset krijgt, krijgt hij dat te zien
 let getItemJSON;
 async function fetchJSONProducten() {
-    // test schema
     try {
         let fetchJSON = await fetch("https://school-project-git-main-yasins-projects-b82e3aad.vercel.app/json/schema.json");
         if (!fetchJSON.ok) {
-            throw new Error("er is een fout opgetreden" + fetchJSON.status);
+            throw new Error("Er is een fout opgetreden" + fetchJSON.status);
         }
         let makeJSON = await fetchJSON.json();
-        // we hebben het gefetcht dus nu gaan we het ophalen en bewerken maar eerst gaan we deze informatie ook oplsaan in localstroage
-        // dus we gaan door met localstorage 1 keer opahelen is genoeg want dit gaat nooit meer gebeuren
-         getItemJSON = JSON.parse(localStorage.getItem("opslaanJSON"));
-        // if(makeJSON){
-        //     getItemJSON = makeJSON;
-        // }
-        // nu gaan we die card ophalen dus  via de admin die gepusht word, word hier weergegeve
+        getItemJSON = JSON.parse(localStorage.getItem("opslaanJSON")) || [];
+
+        if (makeJSON && makeJSON.length > 0) {
+            getItemJSON = makeJSON;
+            localStorage.setItem("opslaanJSON", JSON.stringify(getItemJSON));
+        }
+
+        // Nu de gegevens zijn opgehaald, geef de producten weer in de admin
         if (getItemJSON && getItemJSON.length > 0) {
             productenOphalen();
             winkelmandje(); 
@@ -30,37 +146,39 @@ async function fetchJSONProducten() {
         console.error(error.message);
     }
 }
-// producten ophalen beneden
+
+// Producten ophalen en weergeven
 async function productenOphalen() {
     const HTMLdynamish = document.getElementById("deel");
-        for (let i = 0; i < getItemJSON.length; i++) {
-            let imageURL = getItemJSON[i].url || "../imagess/schema.png";
-            const card = document.createElement("div");
-            card.classList.add("col-md-3");
-            card.innerHTML = `
-          <div class="card">
-              <div class="row g-3">
-                  <div class="col-md-12">
-                      <h3 class="text-center">${getItemJSON[i].name}</h3>
+    for (let i = 0; i < getItemJSON.length; i++) {
+        let imageURL = getItemJSON[i].url || "../imagess/schema.png";
+        const card = document.createElement("div");
+        card.classList.add("col-md-3");
+        card.innerHTML = `
+            <div class="card">
+                <div class="row g-3">
+                    <div class="col-md-12">
+                        <h3 class="text-center">${getItemJSON[i].name}</h3>
                         <div class="col-md-12">
                            <img src="${imageURL}" alt="${getItemJSON[i].name}" class="img-fluid">
                         </div>
-                      <p class="text-center">Prijs: € ${getItemJSON[i].prijs}</p>
-                      <p class="text-center">✔️${getItemJSON[i].vink1}</p>
-                      <p class="text-center">✔️${getItemJSON[i].vink2}</p>
-                     <div class="d-flex justify-content-center">
-                      <button type="button" class="btn btn-warning buttonVanWinkelmandje
-                       text-center col-md-8" data-index="${i}">
-                      In winkelmandje</button></div>
-                  </div>
-              </div>
-          </div>
-      `;
-            HTMLdynamish.appendChild(card);
-        }
+                        <p class="text-center">Prijs: € ${getItemJSON[i].prijs}</p>
+                        <p class="text-center">✔️${getItemJSON[i].vink1}</p>
+                        <p class="text-center">✔️${getItemJSON[i].vink2}</p>
+                        <div class="d-flex justify-content-center">
+                            <button type="button" class="btn btn-warning buttonVanWinkelmandje
+                            text-center col-md-8" data-index="${i}">
+                            In winkelmandje</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        HTMLdynamish.appendChild(card);
+    }
 }
-//? winkelmandje benden 
-// items opgehaald nu de winkelmandje
+
+// Winkelmandje functie
 async function winkelmandje() {
     let winkelmandjeButton = document.querySelectorAll(".buttonVanWinkelmandje");
     winkelmandjeButton.forEach(button => {
@@ -69,7 +187,6 @@ async function winkelmandje() {
             let product = getItemJSON[index];
             let winkelwagen = JSON.parse(localStorage.getItem("winkelwagen")) || [];
             let productIndex = winkelwagen.findIndex(item => item.name == product.name);
-            console.log(productIndex);
             if (productIndex !== -1) {
                 winkelwagen[productIndex].aantal++;
             } else {
@@ -77,9 +194,10 @@ async function winkelmandje() {
                 winkelwagen.push(product);
             }
             localStorage.setItem("winkelwagen", JSON.stringify(winkelwagen));
-            alert(`${product.name} toegevoegd man`);
+            alert(`${product.name} toegevoegd aan je winkelwagen`);
         });
     });
+
     let winkelwagenKnop = document.getElementById("winkelwagenKnop");
     let modalBODY = document.getElementById("modalBODY");
     winkelwagenKnop.addEventListener("click", () => {
@@ -107,8 +225,5 @@ async function winkelmandje() {
         }
     });
 }
+
 fetchJSONProducten();
-function opslaanLOCAL(opslaan) {
-    localStorage.setItem("opslaanJSON", JSON.stringify(opslaan));
-    console.log(JSON.parse(localStorage.getItem("opslaanJSON")));
-}
